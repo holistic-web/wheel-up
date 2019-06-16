734e010dce09a5eee11cff6cebb631a19844471f
michaelfitzhavey <michael.fitzhavey@gmail.com>
2019-06-16 17:37:08 +0100

    chore: setup

--- browser-extension/.eslintrc.js
+++ browser-extension/.eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
+	root: true,
+	env: {
+		node: true
+	},
+	extends: ['plugin:vue/essential', '@vue/airbnb'],
+	rules: {
+		'linebreak-style': 0,
+		'indent': ['error', 'tab'],
+		'no-tabs': 0,
+		'max-len': [1, 160],
+		'comma-dangle': [2, 'never'],
+		'object-curly-newline': 0,
+		'no-new': 0
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	}
 };
